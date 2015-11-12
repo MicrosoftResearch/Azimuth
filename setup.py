5b08010e28a5c34b142bb28ad15fa2bcdb0ea4e1
Nicolo Fusi <fusi@microsoft.com>
2015-11-12 17:43:51 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 
 setup(name='Azimuth',
-      version='0.1',
+      version='0.2',
       author='Nicolo Fusi and Jennifer Listgarten',
       author_email="fusi@microsoft.com, jennl@microsoft.com",
       description=("Machine Learning-Based Predictive Modelling of CRISPR/Cas9 guide efficiency"),
       packages=["azimuth", "azimuth.features", "azimuth.models", "azimuth.tests"],
       install_requires=['scipy', 'numpy', 'matplotlib', 'nose', 'scikit-learn', 'pandas'],
+      license="BSD",
       # ext_modules=cythonize("ssk_cython.pyx"),
       )
