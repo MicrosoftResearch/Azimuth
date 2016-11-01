880042522bbb75c01d5efa218bbb17aa875b4bc8
Nicolo Fusi <fusi@microsoft.com>
2016-11-01 12:40:44 -0400

    added position-dependent model test

--- azimuth/tests/test_saved_models.py
+++ azimuth/tests/test_saved_models.py
@@ -12,11 +12,17 @@ class SavedModelTests(unittest.TestCase):
     This unit test can fail due to randomness in the model (e.g. random seed, feature reordering).
     """
 
-    def test_predictions(self):
-
+    def test_predictions_nopos(self):
         df = pandas.read_csv(os.path.join(dirname, '1000guides.csv'), index_col=0)
         predictions = azimuth.model_comparison.predict(np.array(df['guide'].values), None, None)
-        self.assertTrue(np.allclose(predictions, df['Stable prediction'].values, atol=1e-3))
+        self.assertTrue(np.allclose(predictions, df['truth nopos'].values, atol=1e-3))
 
+    def test_predictions_pos(self):
+        df = pandas.read_csv(os.path.join(dirname, '1000guides.csv'), index_col=0)
+        predictions = azimuth.model_comparison.predict(np.array(df['guide'].values), np.array(df['AA cut'].values), np.array(df['Percent peptide'].values))
+        self.assertTrue(np.allclose(predictions, df['truth pos'].values, atol=1e-3))
+
+
+        
 if __name__ == '__main__':
     unittest.main()
