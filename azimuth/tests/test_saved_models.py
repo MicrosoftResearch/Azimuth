104719f2bae648704474089baa7aaf99b42c338e
Nicolo Fusi <fusi@microsoft.com>
2016-11-01 11:43:16 -0400

    using absolute paths

--- azimuth/tests/test_saved_models.py
+++ azimuth/tests/test_saved_models.py
@@ -3,6 +3,8 @@ import azimuth.model_comparison
 import numpy as np
 import unittest
 import pandas
+import os
+dirname, filename = os.path.split(os.path.abspath(__file__))
 
 class SavedModelTests(unittest.TestCase):
     """
@@ -11,7 +13,8 @@ class SavedModelTests(unittest.TestCase):
     """
 
     def test_predictions(self):
-        df = pandas.read_csv('1000guides.csv', index_col=0)
+
+        df = pandas.read_csv(os.path.join(dirname, '1000guides.csv'), index_col=0)
         predictions = azimuth.model_comparison.predict(np.array(df['guide'].values), None, None)
         self.assertTrue(np.allclose(predictions, df['Stable prediction'].values, atol=1e-3))
 
